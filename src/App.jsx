f1c2d2700b6b747813d46b6b5d79a751162230a3
ValentinBenard <144289468+ValentinBenard@users.noreply.github.com>
2024-02-29 16:32:29 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,13 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Valentin Benard",
+  title: "Développeur Web & Web Mobile",
+  email: "valentin.benard@outlook.com",
+  gitHub: "https://github.com/ValentinBenard",
+  instagram: "/",
+  linkedIn: "/",
+  twitter: "/",
 };
 
 const primaryColor = "#4E567E";
